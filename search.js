33d6f680267e0dd352b70507853285a08a155de7
Seinlin Li <seinlin.li@kaiostech.com>
2018-06-22 17:38:39 +0800

    Include selected text in menu.

--- search.js
+++ search.js
@@ -1,14 +1,14 @@
 const base_url = 'https://www.baidu.com/s?wd=';
 
 browser.contextMenus.create({
-  id: "search-baidu",
-  title: "Search Baidu",
-  contexts: ["selection"]
+  id: 'search-baidu',
+  title: 'Search Baidu for "%s"',
+  contexts: ['selection']
 });
 
 browser.contextMenus.onClicked.addListener(function(info, tab) {
   switch (info.menuItemId) {
-    case "search-baidu":
+    case 'search-baidu':
       console.debug(info.selectionText);
       browser.tabs.create({
         url: base_url + encodeURIComponent(info.selectionText)
