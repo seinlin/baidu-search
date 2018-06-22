fad6dd647d7a7f1884b9aa10c0ea5a3440ed1220
Seinlin Li <seinlin.li@kaiostech.com>
2018-06-22 15:24:15 +0800

    Modify for chrome extension.

--- search.js
+++ search.js
@@ -1,16 +1,17 @@
 const base_url = 'https://www.baidu.com/s?wd=';
 
-browser.contextMenus.create({
-  id: "search-baidu",
-  title: "Search Baidu",
-  contexts: ["selection"]
+chrome.contextMenus.create({
+  id: 'search-baidu',
+  title: 'Search Baidu for "%s"',
+  contexts: ['selection']
 });
 
-browser.contextMenus.onClicked.addListener(function(info, tab) {
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+
   switch (info.menuItemId) {
-    case "search-baidu":
+    case 'search-baidu':
       console.debug(info.selectionText);
-      browser.tabs.create({
+      chrome.tabs.create({
         url: base_url + encodeURIComponent(info.selectionText)
       });
       break;
